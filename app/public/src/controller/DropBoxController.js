8eafb80deca70f1df90d6b7f3198ff294d6c9939
paulowebphp <paulowebphp@gmail.com>
2019-01-28 12:38:10 -0200

    Configurando as informações de tempo restante estimado de download; zerando a barra de rolagem ao fim do download

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -10,6 +10,12 @@ class DropBoxController
 
         this.snackModalEl = document.querySelector('#react-snackbar-root');
 
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+
+        this.namefileEl = this.snackModalEl.querySelector('.filename');
+
+        this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
+
         this.initEvents();
 
     }//END constructor
@@ -34,12 +40,25 @@ class DropBoxController
 
             this.uploadTask(event.target.files);
 
-            this.snackModalEl.style.display = 'block';
+            this.modalShow();
+
+            this.inputFilesEl.value = '';
 
         });//end inputFilesEl.addEventListener
         
 
     }//END initEvents
+
+
+
+
+
+    modalShow( show = true )
+    {
+
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
+
+    }//END modalShow
 
 
 
@@ -63,6 +82,8 @@ class DropBoxController
 
                 ajax.onload = event =>
                 {
+                    this.modalShow(false);
+
                     try
                     {
                         resolve(JSON.parse(ajax.responseText));
@@ -78,13 +99,24 @@ class DropBoxController
 
                 ajax.onerror = event =>
                 {
+                    this.modalShow(false);
+
                     reject(event);
 
                 };//end ajax.onerror
 
+                ajax.upload.onprogress = event =>
+                {
+
+                    this.uploadProgress(event, file);
+
+                };//end ajax.upload.onprogress
+
                 let formData = new FormData();
 
                 formData.append('input-file',file);
+
+                this.startUploadTime = Date.now();
 
                 ajax.send(formData);
 
@@ -98,6 +130,67 @@ class DropBoxController
     }//END uploadTask
 
 
+
+
+
+    uploadProgress( event, file )
+    {
+
+        let timespent = Date.now() - this.startUploadTime;
+
+        let loaded = event.loaded;
+        let total = event.total;
+
+        let porcent = parseInt( ( loaded / total ) * 100 );
+
+        let timeleft = ((100 - porcent) * timespent) / porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`;
+
+        this.namefileEl.innerHTML = file.name;
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft);
+
+    }//END uploadProgress
+
+
+
+
+
+    formatTimeToHuman( duration )
+    {
+
+        let seconds = parseInt((duration/1000) % 60);
+
+        // let minutes = parseInt((duration/(1000*60)) % 60);
+        let minutes = parseInt((duration/(60000)) % 60);
+
+        //let hours = parseInt((duration/(1000*60*60)) % 24);
+        let hours = parseInt((duration/(3600000)) % 24);
+
+        if( hours > 0 )
+        {
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+
+        }//end if
+
+
+        if( minutes > 0 )
+        {
+            return `${minutes} minutos e ${seconds} segundos`;
+
+        }//end if
+
+
+        if( seconds > 0 )
+        {
+            return `${seconds} segundos`;
+
+        }//end if
+
+        return '';
+
+
+    }//END formatTimeToHuman
 
 
 
