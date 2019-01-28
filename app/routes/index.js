83fa7c9482099ed1213d4a8c45a00dcc4e8557ef
paulowebphp <paulowebphp@gmail.com>
2019-01-28 11:19:16 -0200

    Instalando o modulo Formidable no Node.js para realizar o upload de arquivos na pasta upload na raiz do app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+
+/** FORMIDABLE */
+var formidable = require('formidable');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+
+router.post('/upload', (req,res) =>
+{
+
+  let form = new formidable.IncomingForm({
+
+    uploadDir: './upload',
+    keepExtensions: true
+
+  })//end formidable.IncomingForm
+
+  form.parse(req, (err,fields, files) =>
+  {
+
+    res.json({
+
+      /** files: files */
+      files
+
+    });//end res.json
+
+
+  });//end form.parse
+
+  
+
+
+
+
+});//END route
+
+
+
+
+
+/** MODULO EXPORTS */
 module.exports = router;
