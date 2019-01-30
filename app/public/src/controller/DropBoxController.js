20234fefd7675fb1a7d259b09955eac874d889cb
paulowebphp <paulowebphp@gmail.com>
2019-01-30 13:56:12 -0200

    Abrindo arquivos direto do Firebase Storage

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -943,7 +943,7 @@ class DropBoxController
                     break;
             
                 default:
-                    window.open('/file?path='+file.path);
+                    window.open(file.path);
                     break;
 
             }//end switch
